d1620fa294848706c4995e820fc991d655ce6f4d
sannjobs <71654128+sannjobs@users.noreply.github.com>
2021-05-17 22:12:30 +0530

    Update helloworldjavatest.java

--- helloworldjavatest.java
+++ helloworldjavatest.java
@@ -2,6 +2,6 @@ public class helloworldjavatest{
 
 	public static void main(String[] args){
 		
-		System.out.println("Hello World - polling testing by jenkins");
+		System.out.println("Hello World - polling testing by jenkins Babuni");
 	}
 }
